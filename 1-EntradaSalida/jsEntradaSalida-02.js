eee32846c0e807ba028bd2986bbac53c17499425
CordonedaGabriel <gabrielcordoneda@gmail.com>
2020-07-14 13:14:31 -0300

    Clase 14/7

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,25 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var dato
+	dato = prompt("Ingreso dato")
+	alert(dato);
+
+	
+	
+	
+	
+	
+	var nombre;
+	var edad;
+	var apellido;
+	nombre = "GabrielC"
+
+	alert(nombre);
+	edad = "18"
+	apellido = "cordoneda"
+	alert(edad);
+	confirm(apellido);
+	
 }
 
