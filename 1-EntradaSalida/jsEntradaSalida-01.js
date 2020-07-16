84b54898551c3323a9036b1c2eaef2ac25290cb2
CordonedaGabriel <gabrielcordoneda@gmail.com>
2020-07-16 00:56:59 -0300

    clase 15/7

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,16 +3,11 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-<<<<<<< HEAD
 	
 	alert("Gabriel");
 	confirm("apellido");
 	prompt("Ingresar nombre");
 
 
-
-=======
-	alert("Gabriel");
->>>>>>> bec73036936ec49d4e073be96c8dd6b5a0ea7ba4
 }
 
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,41 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var numeroUno;
+	var numeroDos;
+	var suma;
+
+	numeroUno = txtIdNumeroUno.value;
+	numeroDos = txtIdNumeroDos.value;
+
+	numeroUno = parseInt(numeroUno)
+	numeroDos = parseInt(numeroDos)
+
+
+	suma = numeroUno + numeroDos 
+
+
+
+
+
+	//suma = parseInt(numeroUno) + parseInt(numeroDos);
+	alert("La suma es " + suma)
+	 
+	//alert("La suma es " + (parseInt(numeroUno) + parseInt(numeroDos)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
