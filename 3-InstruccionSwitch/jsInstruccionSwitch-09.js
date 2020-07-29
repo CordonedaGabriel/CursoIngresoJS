ce9b208e82b413ed69f7a7e84a953df7f684f858
CordonedaGabriel <gabrielcordoneda@gmail.com>
2020-07-28 22:24:19 -0300

    switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -2,17 +2,68 @@ function mostrar()
 {
 	var estacionIngresada =txtIdEstacion.value;
 	var destinoIngresado =txtIdDestino.value;
-	var estadia = 15000
-	aumento = estadia*20/100
+	var estadia = 15000;
+	var precioTotal;
+	var aumento;
+	var descuento;
 	estadia = parseInt(estadia);
+	aumento = estadia*20/100;
 	aumento = parseInt(aumento);
+	precioTotal = estadia + aumento;
 
 	switch (estacionIngresada){
 		case "Invierno":
 			switch (destinoIngresado){
 				case "Bariloche":
-					alert("el precio es de " + estadia + aumento);
+					alert("el precio es de $" + precioTotal);
+					break;
+				case "Cataratas":
+				case "Cordoba":
+					descuento=estadia*10/100
+					precioTotal = estadia - descuento;
+					alert("el precio es de $" + precioTotal);
+					break;
+				default:
+					descuento=estadia*20/100;
+					precioTotal= estadia - descuento;
+					alert("el precio es de $" + precioTotal);
+					break;
 			}
-	}
+		}switch(estacionIngresada){
+			case "Verano":
+				switch(destinoIngresado){
+					case "Bariloche":
+						descuento= estadia*20/100;
+						precioTotal= estadia - descuento;
+						alert("el precio es de $" + precioTotal);
+						break;
+					case "Cataratas":
+					case "Cordoba":
+						aumento=estadia*10/100;
+						precioTotal=estadia + aumento;
+						alert("el precio es de $" + precioTotal);
+						break;
+					default:
+						aumento=estadia*20/100;
+						precioTotal=estadia + aumento;
+						alert("el precio es de $" + precioTotal);
+						break;
+				}
+		}switch(estacionIngresada){
+			case "Otoño":
+			case "Primavera":
+				switch(destinoIngresado){
+					case "Bariloche":
+					case "Cataratas":
+					case "Mar del plata":
+						aumento=estadia*10/100;
+						precioTotal= estadia + aumento;
+						alert("el precio es de $" + precioTotal);
+						break;
+					default:
+						alert("el precio es de $" + estadia);
+						break;
+				}
+		}
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,49 @@
 function mostrar()
 {
 	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var destinoIngresado =txtIdDestino.value;
+
+	switch(estacionIngresada){
+		case "Invierno":
+			switch (destinoIngresado){
+				case "Bariloche":
+					alert("se viaja");
+					break;
+				default:
+					alert("no se viaja");
+					break;
+			}switch(estacionIngresada){
+				case "Verano":
+					switch (destinoIngresado){
+						case "Mar del plata":
+						case "Cataratas":
+							alert("se viaja");
+							break;
+						default:
+							alert("no se viaja");
+							break;
+					}switch(estacionIngresada){
+						case "Otoño":
+							switch(destinoIngresado){
+								case "Bariloche":
+								case "Cordoba":
+								case "Mar del plata":
+								case "Cataratas":
+									alert("se viaja");
+									break;
+							}switch(estacionIngresada){
+								case "Primavera":
+									switch(destinoIngresado){
+										case "Bariloche":
+											alert("no se viaja");
+											break;
+										default:
+											alert("se viaja");
+											break;
+									}
+							}
+					}
+			}
+	}
 
 }//FIN DE LA FUNCIÓN
